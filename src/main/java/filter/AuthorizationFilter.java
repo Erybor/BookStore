059a8927ccc416ccf27316d1bd5ad26e83bb8a83
Erybor <vmama20@freeuni.edu.ge>
2023-08-17 17:52:21 +0400

    Added an AuthorizationFilter class which is responsible for filtering non-authorized users

---
+++ src/main/java/filter/AuthorizationFilter.java
@@ -0,0 +1,75 @@
+package filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+//@WebFilter("/*")
+public class AuthorizationFilter implements Filter {
+
+    private final List<String> allowedURIs = Arrays.asList("/", "/Login", "/LoginServlet", "/RegisterServlet", "/Register", "/welcome");
+
+    public void init(FilterConfig config) throws ServletException {
+        // Initialization code, if needed
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        String requestedURI = request.getRequestURI();
+
+
+        // Allow css files to be accessed
+
+        if (requestedURI.endsWith(".css")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        if (requestedURI.endsWith(".ico")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        if (requestedURI.contains(".jpg")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        if (requestedURI.contains(".png")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+
+        System.out.println("Requested URI: " + requestedURI);
+        if (allowedURIs.contains(requestedURI)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        boolean loggedIn = request.getSession().getAttribute("username") != null;
+
+        if (loggedIn) {
+            System.out.println("USER IS LOGGED IN");
+            System.out.println(request.getSession().getAttribute("username"));
+            // User is authorized or accessing an allowed URI
+            chain.doFilter(request, response);
+        } else {
+            // User is not authorized, redirect them to the login page or show an error page
+            System.out.println("MEORE");
+//            response.sendRedirect("/Login");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+
+    public void destroy() {
+        // Cleanup code, if needed
+    }
+}
